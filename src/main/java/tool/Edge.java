94734088397fac7ed35bbdb2c9a7f5c097cdc9ca
Nicoleta Gafitanu <nicoleta.gafitanu@virtual7.ro>
2019-05-16 17:08:06 +0300

    Override equals method for edge

--- src/main/java/tool/Edge.java
+++ src/main/java/tool/Edge.java
@@ -53,4 +53,19 @@ public class Edge{
         String Link_prop = Label;
         return (Link_prop);
     }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        // a quick test to see if the objects are identical
+        if (this == otherObject) return true;
+        // must return false if the explicit parameter is null
+        if (otherObject == null) return false;
+        // if the classes don't match, they can't be equal
+        if (!getClass().equals(otherObject.getClass()))
+            return false;
+        // now we know otherObject is a non-null Employee
+        Edge other = (Edge) otherObject;
+        // test whether the fields have identical values
+        return this.source==other.getEdgeSourceNode() &&  this.destination==other.getEdgeDestiantionNode() && this.weight==other.getWeight() ;
+    }
 }

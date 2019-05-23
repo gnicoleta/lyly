6ba1043cc7169a93a74f8c4656d17abc3941169c
Nicoleta Gafitanu <nicoleta.gafitanu@virtual7.ro>
2019-05-23 16:40:18 +0300

    Commented the functionality of the clustering based on the package structure. Parametrized the method for the initial structure, the jar path is given, not hardcoded.

--- src/main/java/tool/InitialPackageStructure.java
+++ src/main/java/tool/InitialPackageStructure.java
@@ -10,12 +10,18 @@ import java.util.zip.ZipInputStream;
 
 public class InitialPackageStructure {
 
-    public  HashMap<String, HashSet<String>> getPackageStructureOfTheSystem() {
+    /**
+     * This method parses the jar of the system and gets the package structure of the system
+     * After getting the packages it gets the classes that those packages contain
+     * Returns a Map composed by the [package name and a list of all contained classes
+     */
+    public HashMap<String, HashSet<String>> getPackageStructureOfTheSystem(String path_to_jar_file) {
         List<String> classNames = new ArrayList<String>();
         Set<String> packages_names = new HashSet<>();
         ZipInputStream zip = null;
         try {
             zip = new ZipInputStream(new FileInputStream("src\\main\\resources\\jars\\manta-1.9.jar"));
+            zip = new ZipInputStream(new FileInputStream(path_to_jar_file));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -43,91 +49,20 @@ public class InitialPackageStructure {
             package_classes.put(s, new HashSet<String>());
         }
 
-        //
         for (String className : classNames) {
             for (String packageName : packages_names) {
-
                 //className is composed by package name + class name
-                //must fin the exact package name in a full class name
+                //must find the exact package name in a full class name
                 //in order to add that class to list of classes contained by a package
                 Pattern p = Pattern.compile(packageName + "\\.([^\\.]+)\\.class");
 
                 //if it is a match, that full class name will be added to list of classes for that package
                 if ((p.matcher(className).matches())) {
                     package_classes.get(packageName).add(className);
-                    //System.out.println("Package: " + packageName + " -- class: " + className);
                 }
             }
         }
-        for (Map.Entry<String, HashSet<String>> e : package_classes.entrySet()) {
-            String key = e.getKey();
-            HashSet<String> value = e.getValue();
-            //System.out.println("Key: " + key);
-            //System.out.println("Val: " + value);
-        }
-
         return package_classes;
     }
-/*
-    public static void main(String[] args) {
-
-        InitialSystemStructure initialSystemStructure = new InitialSystemStructure();
-
-        List<String> classNames = new ArrayList<String>();
-        Set<String> packages_names = new HashSet<>();
-        ZipInputStream zip = null;
-        try {
-            zip = new ZipInputStream(new FileInputStream("src\\main\\resources\\jars\\manta-1.9.jar"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
-                if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-                    String className = entry.getName().replace('/', '.');
-                    classNames.add(className);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for (String s : classNames) {
-            //System.out.println("DDD: " + s.split("\\.([^\\.]+)\\.class").length);
-            packages_names.add(s.split("\\.([^\\.]+)\\.class")[0]);
-        }
-
-        //each package will contain a set with all the classes it contains
-        HashMap<String, HashSet<String>> package_classes = new HashMap<>();
-
-        //put all the packages names as keys in the map
-        for (String s : packages_names) {
-            package_classes.put(s, new HashSet<String>());
-        }
-
-        //
-        for (String className : classNames) {
-            for (String packageName : packages_names) {
-
-                //className is composed by package name + class name
-                //must fin the exact package name in a full class name
-                //in order to add that class to list of classes contained by a package
-                Pattern p = Pattern.compile(packageName + "\\.([^\\.]+)\\.class");
-
-                //if it is a match, that full class name will be added to list of classes for that package
-                if ((p.matcher(className).matches())) {
-                    package_classes.get(packageName).add(className);
-                    System.out.println("Package: " + packageName + " -- class: " + className);
-                }
-            }
-        }
-        for (Map.Entry<String, HashSet<String>> e : package_classes.entrySet()) {
-            String key = e.getKey();
-            HashSet<String> value = e.getValue();
-            System.out.println("Key: " + key);
-            System.out.println("Val: " + value);
-        }
-
-    }*/
 }
 
--- src/main/java/tool/InitialSystemStructure.java
+++ src/main/java/tool/InitialSystemStructure.java
@@ -8,93 +8,81 @@ import edu.uci.ics.jung.graph.util.EdgeType;
 import java.util.*;
 
 public class InitialSystemStructure {
-    Set<Graph> packages_g = new HashSet<>();
+
+    Graph graph;
+
+    public InitialSystemStructure(Graph graph) {
+        this.graph = graph;
+    }
+
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
+
+    public Graph getGraph() {
+        return this.graph;
+    }
+
+    //this will contain all the clusters formed based on the package structure
+    Set<Graph> packages_graph_clusters = new HashSet<>();
+
+    //clustering algorithm instance for coloring the package clusters
+    //computing the MQ
     ClusteringAlgorithm clusteringAlgorithm = new ClusteringAlgorithm();
 
-
-    int contor = 0;
-
-    //TODO: Something is wrong, check to see where is the null pointer exception coming from
-    public Set<Graph>getInitialStructureOfTheSystem(HashMap<String, HashSet<String>> packages_classes_structure, Graph tool_graph) {
-
-        System.out.println("GG" + tool_graph.getEdges());
-
+    public Set<Graph> getInitialStructureOfTheSystem(HashMap<String, HashSet<String>> packages_classes_structure, Graph tool_graph) {
         for (Map.Entry<String, HashSet<String>> e : packages_classes_structure.entrySet()) {
             String key = e.getKey();
             HashSet<String> values = e.getValue();
             ArrayList<Node> nodes = new ArrayList<>();
             ArrayList<Edge> edges = new ArrayList<>();
-
             Graph package_cluster_graph = new UndirectedSparseMultigraph();
-            System.out.println("ASTA E:" + key);
-            System.out.println("ASTA E values:" + values);
 
-            for(String s : values) {
+            for (String s : values) {
                 String node_className = s.substring(0, s.length() - ".class".length());
-                System.out.println("WWW: " + node_className);
                 Node foundNode = clusteringAlgorithm.getNode(tool_graph, new Node(node_className));
-                System.out.println("OOO"+ foundNode);
-                if(foundNode!=null) {
-                    System.out.println("GOOO"+ foundNode);
+                if (foundNode != null) {
                     nodes.add(foundNode);
                 }
             }
-            System.out.println("AAL N: " + nodes);
-            for(Node n : nodes) {
-                System.out.println("EACH: " + tool_graph.getIncidentEdges(n));
+            for (Node n : nodes) {
                 edges.addAll(tool_graph.getIncidentEdges(n));
-                System.out.println("NNN: " + n);
                 package_cluster_graph.addVertex(n);
             }
 
-            System.out.println("EEDE: " + edges);
-            for(Edge edge : edges) {
-                System.out.println("2EEDE: " + edge);
-                if(nodes.contains(edge.getEdgeSourceNode())&& nodes.contains(edge.getEdgeDestiantionNode())) {
+            for (Edge edge : edges) {
+                if (nodes.contains(edge.getEdgeSourceNode()) && nodes.contains(edge.getEdgeDestiantionNode())) {
                     package_cluster_graph.addEdge(edge, edge.getEdgeSourceNode(), edge.getEdgeDestiantionNode(), EdgeType.UNDIRECTED);
                 }
             }
-
-
-            System.out.println("package_cluster_graph: "+ package_cluster_graph);
-
-            packages_g.add(package_cluster_graph);
-
-            //tool_graph.containsVertex()
-
-
+            packages_graph_clusters.add(package_cluster_graph);
         }
-        try {
-            //contor ++;
-            System.out.println("PACK SIZE: " + packages_g.size());
-        } catch (NullPointerException e) {
-            System.out.println("cocos: " + contor);
-        }
-
-        for(Graph g: packages_g) {
-            contor++;
-            System.out.println("AAA:" + contor + "  --------    " + g);
-        }
-        return packages_g;
+        //clusteringAlgorithm.colorClusters(packages_graph_clusters);
+        clusteringAlgorithm.colorNodesInClusters(packages_graph_clusters);
+        return packages_graph_clusters;
     }
 
     public Graph getPackageClustersGraph() {
         Graph g = new UndirectedSparseMultigraph();
-
-        for(Graph ge : packages_g) {
+        for (Graph ge : packages_graph_clusters) {
             Collection<Node> nodes = ge.getVertices();
             Collection<Edge> edges = ge.getEdges();
-            for(Node n : nodes) {
+            for (Node n : nodes) {
                 g.addVertex(n);
             }
 
-            if(edges.size() > 0) {
-                for(Edge e : edges) {
+            if (edges.size() > 0) {
+                for (Edge e : edges) {
                     g.addEdge(e, e.getEdgeSourceNode(), e.getEdgeDestiantionNode(), EdgeType.UNDIRECTED);
                 }
             }
         }
-
+       // clusteringAlgorithm.colorClusters(g, "package");
         return g;
+    }
+
+    public void getPackageClustersMQ() {
+        Double packageSystemMQ = clusteringAlgorithm.computeMQ(this.graph, this.packages_graph_clusters);
+        System.out.println("PACKAGE SYSTEM MQ: " + packageSystemMQ);
     }
 }
